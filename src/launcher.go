809545f8fbf44f9958eb7ea7481c748f6a67d46a
Charvis Yang <charvis.yang@gmail.com>
2017-06-03 19:34:13 +1200

    class mirror

--- src/launcher.go
+++ src/launcher.go
@@ -3,12 +3,24 @@ package main
 import (
 	"io/ioutil"
 	"classfile"
+	"flag"
+	"strings"
 )
 
+var classpath []string
+
 func main()  {
-	bytes, _ := ioutil.ReadFile("/Users/Charvis/Solution.class")
+	var cp = flag.String("classpath", "/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/rt.jar", "Class path")
+	flag.Parse()
+	classpath = strings.Split(*cp, ":")
+
+	bytes, _ := ioutil.ReadFile("Main.class")
 	cr := classfile.NewClassReader(bytes)
 	cf := cr.ReadAsClassFile()
 
-	cf.Print()
+	//cf.Print()
+
+	cm := classfile.NewClassMirror(cf)
+	cm.FindMethod(classfile.SignatureOf("main:V()"))
+	print(1)
 }
