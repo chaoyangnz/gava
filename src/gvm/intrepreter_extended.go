ecbae1b7902cc3ca9c8379fe5d28de492bd19d47
Charvis Yang <charvis.yang@gmail.com>
2017-06-10 02:18:55 +1200

    refine

--- src/gvm/intrepreter_extended.go
+++ src/gvm/intrepreter_extended.go
@@ -9,7 +9,25 @@ func WIDE(opcode uint8, f *StackFrame, t *Thread, c *ClassType, m *Method) {
 
 /*197 (0XC5)*/
 func MULTIANEWARRAY(opcode uint8, f *StackFrame, t *Thread, c *ClassType, m *Method) {
-	panic(fmt.Sprintf("Not implemented for opcode %d\n", opcode))
+	index := m.code[f.pc+1] << 8 | m.code[f.pc+2]
+	dimensions := m.code[f.pc+3]
+
+	arrayType := c.constantPool[index].resolve().(*RuntimeConstantClassInfo).referenceType.(*ArrayType)
+	componentType := arrayType.componentType.(ReferenceType)
+	counts := make([]t_int, dimensions)
+	for i := t_int(dimensions-1); i >= 0; i-- {
+		counts[i] = f.pop().(t_int)
+	}
+	var array *t_array
+	componentArray := newArray(componentType, counts[0])
+	for j := uint8(1); j < dimensions-1; j++ {
+		array = newArray(componentType, counts[j])
+		for k := t_int(0); k < counts[j]; k++ {
+			array.elements[k] = componentArray
+		}
+		componentArray = array
+	}
+	f.push(array)
 }
 
 /*198 (0XC6)*/
